3aa157b7b9ad3895738be20a909b18b717b8cc31
ShardulAswale <unrealdoc12@gmail.com>
2023-07-28 22:52:35 +0530

    Fix:

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Link, Outlet } from "react-router-dom";
 
@@ -7,12 +5,12 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        {/* <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        </a> */}
       </div>
       <h1>Vite + React</h1>
 
@@ -24,7 +22,9 @@ function App() {
 
       <Outlet />
 
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   );
 }
